94c75412d13206f3eb0575290fa89a9f8a23f246
nameon <nameonb0507@gmail.com>
2021-01-07 14:38:52 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-import './App.css';
 import Customer from './components/Customer';
+import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: "auto" //x축 오버플로우 발생 처리
+    },
+    table: {
+        minWidth: 1080 //화면 크기가 줄어도 1080픽셀만큼 테이블 크기 정해짐
+    }
+})
 
 const customers = [
 {
@@ -32,38 +49,27 @@ const customers = [
 
 class App extends Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                {/* <Customer
-                    id={customers[0].id}
-                    image={customers[0].image}
-                    name={customers[0].name}
-                    birthday={customers[0].birthday}
-                    gender={customers[0].gender}
-                    job={customers[0].job}
-                />
-                <Customer
-                    id={customers[1].id}
-                    image={customers[1].image}
-                    name={customers[1].name}
-                    birthday={customers[1].birthday}
-                    gender={customers[1].gender}
-                    job={customers[1].job}
-                />
-                <Customer
-                    id={customers[2].id}
-                    image={customers[2].image}
-                    name={customers[2].name}
-                    birthday={customers[2].birthday}
-                    gender={customers[2].gender}
-                    job={customers[2].job}
-                /> */}
-                { // 반복문으로 배열 소스 compact
-                    customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job}/>);})
-                }
-            </div>
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>이미지</TableCell>
+                            <TableCell>이름</TableCell>
+                            <TableCell>생년월일</TableCell>
+                            <TableCell>성별</TableCell>
+                            <TableCell>직업</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job} />); })}
+                    </TableBody>
+                </Table>
+            </Paper>
         );
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
