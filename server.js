0ee2a2e47f25a9ebf7c4d4e143877644a61ad201
nameon <nameonb0507@gmail.com>
2021-01-08 16:43:24 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '오키연구소',
-            'birthday': '955555',
-            'gender': '남자',
-            'job': '연구원'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': 'KNE',
-            'birthday': '960508',
-            'gender': '남자',
-            'job': '프로그래머'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '개발자',
-            'birthday': '961127',
-            'gender': '남자',
-            'job': '이사'
+    connection.query(
+        'SELECT * FROM CUSTOMER',
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
